143fa4a709b3a4b8c940a531032d60887a544bae
Mateusz <wmateusz993@gmail.com>
2023-02-27 19:15:35 +0100

    Start project, working on home page add navbar

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './App.css';
+import { HomePage } from './layouts/navbar/HomePage';
+import { Navbar } from "./layouts/navbar/Navbar";
+
+function App() {
+  return (
+    <div>
+<Navbar/>
+<HomePage/>
+</div>
+  );
+}
+
+export default App;
---
+++ src/layouts/navbar/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+
+export const HomePage = () =>{
+    return (
+    
+        <div className='p-5 mb-4 bg-dark header'>
+            <div className='container-fluid py-5 text-white d-flex justify-content-center align-items-center'>
+                <div>
+                    <h1 className='display-5 fw-bold'>Your private BookShlef</h1>
+                    <p className='col-md-8 fs-4'>What would you like to put next?</p>
+                
+                </div>
+            </div>
+        </div>
+    );
+}
