9bbccd7348647fb709f12314fb155016d408a3e0
Mateusz <wmateusz993@gmail.com>
2023-02-28 12:06:47 +0100

    Add carousel still working on

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 
 import './App.css';
+import BookModel from './models/BookModel';
 import { Footer } from './layouts/Footer';
 import { HomePage } from './layouts/navbar/HomePage';
 import { Navbar } from "./layouts/navbar/Navbar";
+import { Carousel } from './layouts/Carousel';
 
 function App() {
   return (
     <div>
 <Navbar/>
 <HomePage/>
+<Carousel/>
 <Footer/>
+
 </div>
   );
 }
